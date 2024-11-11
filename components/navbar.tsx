80b60320665c712607c12ec3bf51431a463fa943
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-11-11 19:32:48 +0800

    new

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
+import Image from "next/image";
 export const revalidate = 0;
 
 const Navbar = async () => {
@@ -13,7 +14,13 @@ const Navbar = async () => {
         <div className="border-b sticky top-0 left-0 z-30 backdrop-blur-sm bg-slate-100/5 dark:bg-white/5 border-slate-600/40 dark:border-white/40">
             <Container>
                 <div className="relative px-0 sm:px-6 lg:px-8 flex h-16 items-center">
-                    <Link href="/" className="ml-4 flex lg:ml-0     gap-x-2">
+                    <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                        <Image
+                            alt="logo"
+                            src='/streetheus-logo.png'
+                            width={100}
+                            height={30}
+                        />
                         <p className="font-bold text-xl relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500">STORE</p>
                     </Link>
                     <MainNav data={categories} />
