8e210468ffdcbe069e47ce5a2cb0ed80cf11e050
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-11-23 09:34:59 +0800

    added

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -24,7 +24,7 @@ const ProductPage: React.FC<ProductPagesProps> = async ({
             <div className="top-1 absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             <Container>
                 <div className="px-4 py-10 sm:px-6 lg:px-8">
-                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">b
+                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
                         <Gallery images={product.images} />
                         <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
                             <Info data={product} />
