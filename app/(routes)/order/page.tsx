09746fad6d6e9ee1a9722a2bda878851549f87f0
ivanprejoles <ivanpatrickprejoles@gmail.com>
2024-12-04 07:18:38 +0800

    added

--- app/(routes)/order/page.tsx
+++ app/(routes)/order/page.tsx
@@ -1,59 +1,69 @@
 import { format } from "date-fns";
-import getOrders from '@/actions/get-orders'
-import { auth } from '@clerk/nextjs'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import { OrderClient } from './components/client'
-import { OrderColumn, PaymentColumn } from './components/columns'
+import getOrders from "@/actions/get-orders";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
+import { OrderClient } from "./components/client";
+import { OrderColumn, PaymentColumn } from "./components/columns";
 import { formatter } from "@/lib/utils";
 
 const OrderPage = async () => {
-    const {userId} = auth()
+  const { userId } = auth();
 
-    if (!userId) {
-        redirect('/sign-in');
-    }
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
-    const orders = await getOrders({userId})
+  const orders = await getOrders({ userId });
 
-    const formattedOrders: OrderColumn[] = orders.map((item: any) => ({
-        id: item.id,
-        email: item.email,
-        name: item.name,
-        phone: item.phone,
-        address: item.address,
-        products: item.orderItems.map((orderItem: any) => orderItem.product.name).join(', '),
-        totalPrice: formatter.format(item.orderItems.reduce((total: any, item: any) => {
-            return total + Number(item.product.price)
-        }, 0)),
-        isPaid: item.isPaid,
-        createdAt: format(item.createdAt, "MMMM do, yyyy")
+  const formattedOrders: OrderColumn[] = orders.map((item: any) => ({
+    id: item.id,
+    email: item.email,
+    name: item.name,
+    phone: item.phone,
+    address: item.address,
+    products: item.orderItems
+      .map((orderItem: any) => orderItem.product.name)
+      .join(", "),
+    totalPrice: formatter.format(
+      item.orderItems.reduce((total: any, item: any) => {
+        return total + Number(item.product.price);
+      }, 0)
+    ),
+    isPaid: item.isPaid,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  const allPaymentDetails: PaymentColumn[] = orders.flatMap((order: any) =>
+    order.payments.map((payment: any) => ({
+      id: payment.id,
+      orderId: payment.orderId,
+      phone: payment.phone,
+      address: payment.address,
+      products: order.orderItems
+        .map((orderItem: any) => {
+          const sizes = orderItem.sizes.join(", ");
+          const colors = orderItem.colors.join(", ");
+          return `${orderItem.product.name} (${orderItem.quantity}) [${sizes} | ${colors}]`;
+        })
+        .join(", "),
+      email: payment.email,
+      name: payment.name,
+      date: format(payment.date, "MMM do, yyyy"),
+      amount: formatter.format(Number(payment.amount)),
+      imageSrc: payment.imageSrc,
+      createdAt: format(order.createdAt, "MMMM do, yyyy"),
     }))
-    
-    const allPaymentDetails: PaymentColumn[] = orders.flatMap((order: any) =>
-        order.payments.map((payment: any) => ({
-            id: payment.id,
-            orderId: payment.orderId,
-            phone: payment.phone,
-            address: payment.address,
-            products: order.orderItems.map((orderItem: any) => orderItem.product.name).join(', '),
-            email: payment.email,
-            name: payment.name,
-            date: format(payment.date, "MMM do, yyyy"),
-            amount: formatter.format(Number(payment.amount)),
-            imageSrc: payment.imageSrc,
-            createdAt: format(order.createdAt, "MMMM do, yyyy")
-        }))
-    );
-    
-  return (
-    <div className="flex-col bg-white dark:bg-[#020817] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] min-h-screen">
-        <div className="absolute top-0 pointer-events-none inset-0 w-full h-full flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <div className="flex-1 space-y-4 p-8 pt-6">
-            <OrderClient data={formattedOrders} payment={allPaymentDetails} />
-        </div>
-    </div>
-  )
-}
+  );
 
-export default OrderPage
+  return (
+    <div className="dark:bg-black bg-white dark:bg-grid-small-orange/[0.2] bg-grid-small-rose-400/95 relative">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <OrderClient data={formattedOrders} payment={allPaymentDetails} />
+      </div>
+    </div>
+  );
+};
+
+export default OrderPage;
